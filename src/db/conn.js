f4b61476e9830aef479de49580bc63a1f6c36239
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-01-22 13:53:58 +0530

    Whatsapp-api

---
+++ src/db/conn.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+const DB = process.env.SecretKey;
+
+mongoose.set("strictQuery", true);
+mongoose.connect(DB).then(() => {
+    console.log("connection is successful");
+  })
+  .catch((e) => {
+    console.log("no connection");
+  });
